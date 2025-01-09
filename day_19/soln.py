e64e45a274f29b6c33d5fe15c88d1d91428d41f2
drew ellingson <drew.ellingson@gmail.com>
2025-01-09 13:39:04 -0800

    fun word reduction problem with caching

--- day_19/soln.py
+++ day_19/soln.py
@@ -3,18 +3,18 @@ from functools import lru_cache
 
 
 @lru_cache
-def count_ways_one(available: Tuple[str], pattern: str) -> Optional[int]:
+def count_ways_one(available: Tuple[str, ...], pattern: str) -> Optional[int]:
     if not pattern:
         return 1
     else:
         cands = [pattern.replace(a, "", 1) for a in available if pattern.startswith(a)]
-        return sum(count_ways_one(available, c) for c in cands)
+        return sum(count_ways_one(available, c) or 0 for c in cands)
 
 
 def count_ways_all(
-    available: Tuple[str], patterns: Tuple[str], p2: bool = False
+    available: Tuple[str, ...], patterns: Tuple[str, ...], p2: bool = False
 ) -> int:
-    counts = [count_ways_one(available, p) for p in patterns]
+    counts = [count_ways_one(available, p) or 0 for p in patterns]
     return len([c for c in counts if c > 0]) if not p2 else sum(counts)
 
 
