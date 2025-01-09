80e842067cc6a788b8cdf541f3536db7d72a6c83
drew ellingson <drew.ellingson@gmail.com>
2025-01-09 10:52:32 -0800

    fun word reduction problem with caching

--- day_19/soln.py
+++ day_19/soln.py
@@ -1,37 +1,25 @@
-from dataclasses import dataclass
-from typing import List 
+from typing import Tuple
+from functools import lru_cache
 
 
-@dataclass
-class TowelSet:
-    available: List[str]
-    patterns: List[str]
-
-    def is_possible(self, pattern):
-        if pattern in self.available:
-            return True 
-        else:
-            cands = [pattern.replace(a, '', 1) for a in self.available if pattern.startswith(a)]
-            return any(self.is_possible(c) for c in cands)
-
-    def possible_count(self):
-        # count = 0
-        # for i, p in enumerate(self.patterns):
-        #     if self.is_possible(p):
-        #         count += 1
-        #     print(f'done {i} / {len(self.patterns)} for {100*round(i / len(self.patterns), 4)}')
-        # return count 
-    
-        return len([p for p in self.patterns if self.is_possible(p)])
+@lru_cache
+def count_ways_one(available: Tuple[str], pattern: str) -> bool:
+    if not pattern:
+        return 1
+    else:
+        cands = [pattern.replace(a, "", 1) for a in available if pattern.startswith(a)]
+        return sum(count_ways_one(available, c) for c in cands)
 
 
-with open('day_19/input_sample.txt') as f:
-    available, patterns = f.read().split('\n\n')
+def count_ways_all(available: Tuple[str], patterns: Tuple[str], p2: bool = False):
+    counts = [count_ways_one(available, p) for p in patterns]
+    return len([c for c in counts if c > 0]) if not p2 else sum(counts)
 
-    available = available.strip().split(', ')
-    patterns = patterns.split('\n')
 
-ts = TowelSet(available, patterns)
+with open("day_19/input.txt") as f:
+    available, patterns = f.read().split("\n\n")
+    available = tuple(available.strip().split(", "))
+    patterns = tuple(patterns.split("\n"))
 
-print(ts)
-print(ts.possible_count())
+print(f"P1 Soln is: {count_ways_all(available, patterns)}")
+print(f"P2 Soln is: {count_ways_all(available, patterns, p2=True)}")
