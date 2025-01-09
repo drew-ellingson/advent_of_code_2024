69d6ca5bbd60d3622cdd7f90351e95a1bb8f7835
drew ellingson <drew.ellingson@gmail.com>
2025-01-09 10:55:09 -0800

    fun word reduction problem with caching

--- day_19/soln.py
+++ day_19/soln.py
@@ -11,7 +11,7 @@ def count_ways_one(available: Tuple[str], pattern: str) -> bool:
         return sum(count_ways_one(available, c) for c in cands)
 
 
-def count_ways_all(available: Tuple[str], patterns: Tuple[str], p2: bool = False):
+def count_ways_all(available: Tuple[str], patterns: Tuple[str], p2: bool = False) -> int:
     counts = [count_ways_one(available, p) for p in patterns]
     return len([c for c in counts if c > 0]) if not p2 else sum(counts)
 
